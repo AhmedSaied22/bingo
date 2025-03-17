d091f37a402c4c73b5c9efd18e205e4919421460
AhmedSaied22 <ahmedsaied2016201@gmail.com>
2025-03-17 16:48:23 +0200

    update files

--- lib/features/home/presentation/views/widgets/mobile_home_view.dart
+++ lib/features/home/presentation/views/widgets/mobile_home_view.dart
@@ -1,18 +1,24 @@
+import 'package:bingo/core/theme/theme.dart';
 import 'package:bingo/core/ui/app_colors.dart';
 import 'package:bingo/core/ui/widgets/main_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MobileHomeView extends StatelessWidget {
   const MobileHomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Text('BINGO'),
-        MainButton(title: 'Join a game', color: AppColors.secondaryColor),
-        MainButton(title: 'Create a game', color: AppColors.primaryColor),
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 20.h,
+        children: [
+          Text('BINGO',style: AppTheme.,),
+          MainButton(title: 'Join a game', color: AppColors.secondaryColor),
+          MainButton(title: 'Create a game', color: AppColors.primaryColor),
+        ],
+      ),
     );
   }
 }
